802e441908229677b85844fefba975439b50db16
iftikharalideveloper <iftikharali.developer@gmail.com>
2026-08-13 00:27:16 +0500

    fix mongodb connection for deployment

--- database/database.js
+++ database/database.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
-import dns from "dns";
 
-dns.setServers(["8.8.8.8", "4.4.8.8"]);
+export const connDB = async () => {
+  const urlDB = process.env.DATABASE_URL;
 
-export const connDB = async (req, res) =>{
-    const urlDB = process.env.DATABASE_URL;
-    try {
-        await mongoose.connect(urlDB);
-        console.log("Database Connected Successfully!");
-    } catch (error) {
-        console.log("Failed to Connect DB : ", error.message);
-    }
-}
+  if (!urlDB) {
+    throw new Error("DATABASE_URL is not defined");
+  }
+
+  try {
+    await mongoose.connect(urlDB);
+
+    console.log("Database Connected Successfully!");
+  } catch (error) {
+    console.error("Failed to Connect DB:", error.message);
+
+    throw error;
+  }
+};
