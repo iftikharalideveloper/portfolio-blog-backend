e25d976aa06f4ded5a404066c38e378f9ffc2673
iftikharalideveloper <iftikharali.developer@gmail.com>
2026-08-12 22:43:25 +0500

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+
 import { connDB } from "./database/database.js";
-// auth Routes
+
+// Auth Routes
 import authRoutes from "./routes/auth.routes.js";
-// post routes
+
+// Post Routes
 import postRoutes from "./routes/post.routes.js";
-//comment routes
+
+// Comment Routes
 import commentRoutes from "./routes/comment.routes.js";
-//project routes
+
+// Project Routes
 import projectRoutes from "./routes/project.routes.js";
-// api messages
+
+// Message Routes
 import messageRoutes from "./routes/message.routes.js";
 
-
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
@@ -23,15 +29,15 @@ app.use(morgan("dev"));
 app.use(cors());
 
 connDB();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/messages", messageRoutes);
 
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port: http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port: ${port}`);
 });
